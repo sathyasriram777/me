33e904130125dee24356f1a7904b9f3ccdfce008
Sathya <98763491+sathyasriram777@users.noreply.github.com>
2025-10-24 01:37:04 -0400

    resume

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,13 @@
+export default function ResumePage() {
+  return (
+    <div className="fixed inset-0 w-full h-full">
+      <iframe
+        src="Sathya_Sriram_Resume.pdf"
+        width="100%"
+        height="100%"
+        className="border-0"
+        title="Resume PDF"
+      />
+    </div>
+  );
+}
